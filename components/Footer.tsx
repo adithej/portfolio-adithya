7edbd63350c7a6b3fc2d3c286ff54abfeb83d399
Adithya Thejas <adithyathejass.1997@gmail.com>
2024-07-08 15:50:47 +0530

    footer fix and svg size

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import MagicButton from "./ui/MagicButton";
 import { FaLocationArrow } from "react-icons/fa";
 import { socialMedia } from "@/data";
+import Link from "next/link";
 
 const Footer = () => {
   return (
@@ -34,12 +35,14 @@ const Footer = () => {
               className="w-10 h-10 cursor-pointer flex justify-center items-center 
             backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"
             >
-              <img
-                src={profile.img}
-                alt="profile image"
-                width={20}
-                height={20}
-              />
+              <Link href={profile.link} target="_blank">
+                <img
+                  src={profile.img}
+                  alt="profile image"
+                  width={20}
+                  height={20}
+                />
+              </Link>
             </div>
           ))}
         </div>
